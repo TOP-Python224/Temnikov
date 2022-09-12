31e38d6a13fb22d353f93533bbbb90adfbbd55a9
Arisber73 <atemnikow@yandex.ru>
2022-09-12 19:11:49 +0400

    HW 09.05

---
+++ 09.05/2.py
@@ -0,0 +1,24 @@
+string = input(' > ')
+binary = {'0', '1', 'b'}
+
+if set(string).issubset(binary):
+	print('ДА')
+else:
+	print('НЕТ')
+
+
+# stdout:
+#  > 0100101
+# ДА
+
+#  > b0110
+# ДА
+
+#  > 0b01001110
+# ДА
+
+#  > 0ac1201001
+# НЕТ
+
+#  > 123
+# НЕТ
